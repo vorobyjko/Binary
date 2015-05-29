b1ba6fd3815caf7368d77fe34d51b951d2ee95f3
unknown <kostiantyn.bogapov@KBP1-DNN-18132.synapse.com>
2015-05-29 16:40:11 +0300

    javascript improvemetns

--- JavaScript/js/scripts.js
+++ JavaScript/js/scripts.js
@@ -2,96 +2,75 @@
 
 var Man_object_create = {
 	constructor : function (name, age) {
-		this.name = name || "Вася";
-		this.age = age || "24";
-		this.live = function () {};
+		this.name = name || "Unnamed Man";
+		this.age = age || "99";
 		return this;
+	},
+	live : function () {}
+};
+
+var Student_object_create = Object.create(Man_object_create, {
+	constructor : { 
+		value : function(){ 
+			Man_object_create.constructor.apply(this, arguments);
+			return this; 
+		}
+	},
+	study : { 
+		value : function () {} 
 	}
-};
-
-var man_object_create = Object.create(Man_object_create).constructor("Аня", "19");
-
-var Student_object_create = Object.create(Man_object_create).constructor("John", "19");
-
-Student_object_create.constructor = function () {
-	Student_object_create.constructor.apply(this, arguments);
-}
-
-var student_obj_create = Student_object_create;
-
-//Man_object_create.prototype.live = 
-
-/*var Student_object_create = function () {
-	
-};
-
-Student_object_create.prototype = Object.create(Man_object_create.__proto__);
-Student_object_create.prototype.constructor = Student_object_create;
-
-
-Student_object_create.prototype.study = function () {
-		
-};*/
-
-
-
+});
 
 
 /* Function-constructor inheritance  */
 
 var Man_function_constructor = function (name, age) {
-	this.name = name || "Вася";
-	this.age = age || "24";
+	this.name = name || "Unnamed Man";
+	this.age = age || "99";
+};
 
-	this.live = function () {
+Man_function_constructor.prototype.live = function () {
 
-	}
-}
+};
 
 var Student_function_constructor = function () {
-	this.study = function () {
-
-	}
-}
+	Man_function_constructor.apply(this, arguments);
+};
 
 Student_function_constructor.prototype = new Man_function_constructor();
 
+Student_function_constructor.prototype.study = function () {
+
+};
+
+
+
+/* duckType implementation */
 
 function duckType (obj) {
 	if (!obj || (typeof obj !== "object")) {
 		console.error("Wrong data type or expected parameter is empty!");
 	} else {
-		for (var prop in obj) {
-			if ((obj.hasOwnProperty(prop)) && (typeof obj[prop] === "function")) {
-				switch (prop) {
-					case "live" : console.log("Type of the Object: Man");
-						break;
-					case "study" : console.log("Type of the Object: Student");
-						break;
-					default : console.error("Can't find the type");
-				}
-			}		
-		};
+		if (("live" in obj) && ("study" in obj)) {
+			console.log("Type of the Object: Student");
+		} else {
+			console.log("Type of the Object: Man");
+		}
 	}
 };
 
 
+/* duckTypeModify implementation */
 
 function duckTypeModify () {
 	if (this === window) {
 		console.error("Function duckTypeModify can't be running in global context. Please set the context!");
 	} else {
-		for (var prop in this) {
-			if ((this.hasOwnProperty(prop)) && (typeof this[prop] === "function")) {
-				switch (prop) {
-					case "live" : console.log("Type of the Object: Man");
-						break;
-					case "study" : console.log("Type of the Object: Student");
-						break;
-					default : console.error("Can't find the type");
-				}
-			}		
-		};
+		if (("live" in this) && ("study" in this)) {
+			console.log("Type of the Object: Student");
+		} else {
+			console.log("Type of the Object: Man");
+		}
 	}
 };
 
@@ -99,11 +78,12 @@ function duckTypeModify () {
 
 /* Creating instances of constructors */
 
-var man_fun_const = new Man_function_constructor("Виталий", "21");
-var student_fun_const = new Student_function_constructor();
+var man_fun_const = new Man_function_constructor("Vitaly", "21");
+var student_fun_const = new Student_function_constructor("Elena", "20");
 
-//var man_obj_create = new Man_object_create("Анна", "19");
-//var student_obj_create = new Student_object_create();
+var man_object_create = Object.create(Man_object_create).constructor("Andrey", "22");
+var student_object_create = Object.create(Student_object_create).constructor("Anna", "19");
+
 
 
 
@@ -126,17 +106,21 @@ console.log(man_object_create.name);
 console.log(man_object_create.age);
 
 console.info("Object-create style - Student Object:");
-console.log(student_obj_create);
-console.log(student_obj_create.name);
-console.log(student_obj_create.age);
+console.log(student_object_create);
+console.log(student_object_create.name);
+console.log(student_object_create.age);
 
 
 console.info("Running duckType");
 duckType(student_fun_const);
 duckType(man_fun_const);
+duckType(student_object_create);
+duckType(man_object_create);
 duckType();
 
 console.info("Running duckTypeModify");
 duckTypeModify.call(student_fun_const);
 duckTypeModify.call(man_fun_const);
+duckTypeModify.call(student_object_create);
+duckTypeModify.call(man_object_create);
 duckTypeModify.call();
